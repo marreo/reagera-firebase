90592acb39abae7fcc3ecd0b3f2831b8f47ccba5
Martin Lindgren <martin@marreomedia.se>
2018-09-22 19:13:08 +0200

    Init

--- android/app/src/main/java/com/marreomedia/reageratest/MainApplication.java
+++ android/app/src/main/java/com/marreomedia/reageratest/MainApplication.java
@@ -1,4 +1,4 @@
-package com.invertase.rnfirebasestarter;
+package com.marreomedia.reageratest;
 
 import android.annotation.SuppressLint;
 import android.app.Application;
--- ios/reageratestTests/reageratestTests.m
+++ ios/reageratestTests/reageratestTests.m
@@ -14,11 +14,11 @@
 #define TIMEOUT_SECONDS 600
 #define TEXT_TO_LOOK_FOR @"Welcome to React Native!"
 
-@interface RNFirebaseStarterTests : XCTestCase
+@interface reageratestTests : XCTestCase
 
 @end
 
-@implementation RNFirebaseStarterTests
+@implementation reageratestTests
 
 - (BOOL)findSubviewInView:(UIView *)view matching:(BOOL(^)(UIView *view))test
 {
