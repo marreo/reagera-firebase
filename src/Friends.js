502cd823e529a1d77fef80e6073257c81378f98c
Magnus J <mange_johanson@hotmail.com>
2018-11-04 13:10:19 +0100

    Added friends header

--- src/Friends.js
+++ src/Friends.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import { View, Text, ActivityIndicator, StyleSheet, FlatList, Image, Button } from 'react-native'
-import { List, ListItem } from 'react-native-elements'
+import { List, ListItem, Header } from 'react-native-elements'
 
 const list = [{
   name: 'Amy Farha',
@@ -45,69 +45,95 @@ export default class Loading extends React.Component {
   componentDidMount() {
   }
 
-  GetFriendList = async function(){
-    // var test = getMoviesFromApi();
-    var test2 = await fetch('https://reagera-api.herokuapp.com/api/friend', {  
+  GetUserList = async function(){
+    var response = await fetch('https://reagera-api.herokuapp.com/api/user', {  
+      method: 'GET',
+      headers: {
+        'Accept': 'application/json',
+        'Content-Type': 'application/json',
+      }
+    })
+
+    let message = await response.json();
+    this.state.data = message;
+    this.setState(message);
+  }
+
+  pressFriend = async function(id, name) {
+    var response = await fetch('https://reagera-api.herokuapp.com/api/friend/create', {  
       method: 'POST',
       headers: {
         'Accept': 'application/json',
         'Content-Type': 'application/json',
       },
       body: JSON.stringify({
-        userid: '5bddc17e71f0280015ea90e9'
+        f1: '5bddc17e71f0280015ea90e9',
+        f2: id
       })
     })
-
-    let message = await test2.json();
-    this.state.data = message;
   }
 
   render() {
     return (
-      <View style={styles.container}>
-        <Text>My Friends .</Text>
-        <List containerStyle={{ marginBottom: 20 }}>
-          {
-            list.map((l) => (
-              <ListItem
-                roundAvatar
-                avatar={{uri:l.avatar_url}}
-                key={l.name}
-                title={l.name}
-              />
-            ))
-          }
-        </List>
-        <FlatList
-          style={{alignSelf: 'stretch', flex: 1, flexDirection: 'column'}}
-          data={this.state.data}
-          renderItem={
-            ({item}) => 
-            <View style={{alignSelf: 'stretch', backgroundColor: 'white', padding: 10, flex: 2, flexDirection: 'row'}}>
-              <View style={{flex: 1, flexDirection: 'column'}}>
-                <Image
-                  style={{width: 50, height: 50, borderRadius: 75}}
-                  source={require('../assets/user2.jpg')}
-                />
-              </View>
-              <View style={{
-                flex: 5, 
-                alignContent: 'flex-start', 
-                flexDirection: 'column', 
-                justifyContent: 'center'
-              }}>
-                <Text style={{
-                  fontSize: 18
-                }}>
-                  {item.name}
-                </Text>
-              </View>
-            </View>
-          }
+      <View style={{flex: 1}}>
+        <Header
+          style={{flex: 9, flexDirection: 'column', alignSelf: 'stretch'}}
+          leftComponent={{ icon: 'menu', color: '#fff' }}
+          centerComponent={{ text: 'Users', style: { color: '#fff', fontSize: 22 } }}
+          rightComponent={{ icon: 'home', color: '#fff' }}
         />
-        <Button
-          onPress={() => this.GetFriendList()} 
-          title="Get friends list"/>
+        <View style={styles.container}>
+        {/* <Text style={{fontSize: 20}}>All users</Text> */}
+        <View style={{flex: 9, flexDirection: 'column', alignSelf: 'stretch'}}>
+          <List containerStyle={{ marginBottom: 20, alignSelf: 'stretch' }}>
+            {
+              this.state.data.map((l, i) => (
+                <ListItem
+                  roundAvatar
+                  avatar={require('../assets/user2.jpg')}
+                  key={i}
+                  title={l.name}
+                  subtitle={l.email}
+                  onPress={() => this.pressFriend(l._id, l.name)}
+                />
+              ))
+            }
+          </List>
+          {/* <FlatList
+            style={{alignSelf: 'stretch', flex: 1, flexDirection: 'column'}}
+            data={this.state.data}
+            renderItem={
+              ({item}) => 
+              <View style={{alignSelf: 'stretch', backgroundColor: 'white', padding: 10, flex: 2, flexDirection: 'row', borderBottomWidth: 1}}>
+                <View style={{flex: 1, flexDirection: 'column'}}>
+                  <Image
+                    style={{width: 50, height: 50, borderRadius: 75}}
+                    source={require('../assets/user2.jpg')}
+                  />
+                </View>
+                <View style={{
+                  flex: 5, 
+                  alignContent: 'flex-start', 
+                  flexDirection: 'column', 
+                  justifyContent: 'center'
+                }}>
+                  <Text style={{
+                    fontSize: 18
+                  }}>
+                    {item.name}
+                  </Text>
+                  <Text>{item.email}</Text>
+                </View>
+              </View>
+            }
+          /> */}
+        </View>
+        <View style={{flex: 1}}>
+          <Button
+            onPress={() => this.GetUserList()} 
+            title="Get user list"/>
+        </View>
+        </View>
       </View>
     )
   }
--- src/Me.js
+++ src/Me.js
@@ -2,9 +2,35 @@ import React from 'react'
 import { View, Text, ActivityIndicator, StyleSheet, Button, Image, TextInput, Alert } from 'react-native'
 import { Header } from 'react-native-elements'
 import Ionicons from 'react-native-vector-icons/Ionicons'
+import { List, ListItem } from 'react-native-elements'
 
 export default class Loading extends React.Component {
+  constructor(){
+    super();
+
+    this.state = {
+      data: []
+    }
+  }
+
   componentDidMount() {
+  }
+
+  GetFriendList = async function(){
+    var response = await fetch('https://reagera-api.herokuapp.com/api/friend', {  
+      method: 'POST',
+      headers: {
+        'Accept': 'application/json',
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({
+        userid: '5bddc17e71f0280015ea90e9'
+      })
+    })
+  
+    let message = await response.json();
+    this.state.data = message;
+    this.setState(message);
   }
 
   render() {
@@ -21,16 +47,38 @@ export default class Loading extends React.Component {
         }}
       />
         <View style={styles.container}>
-          <Image
-            style={{width: 150, height: 150, borderRadius: 75}}
-            source={require('../assets/user2.jpg')}
-          />
-          <Text style={{fontSize: 16}}>Name</Text>
-          <TextInput style={{fontSize: 30}}>T</TextInput>
-          <Button
-            onPress={() => this.props.navigation.navigate('Friends')} 
-            title="Add friend"/>
-        </View>
+        <Image
+          style={{width: 150, height: 150, borderRadius: 75}}
+          source={require('../assets/user2.jpg')}
+        />
+        <Text style={{fontSize: 20}}>Name</Text>
+        <TextInput 
+          style={{
+            fontSize: 30, 
+            alignSelf: 'center', 
+            textAlign: 'center',
+            minWidth: 90
+          }} 
+          placeholder="Name">
+        </TextInput>
+
+        <Text>My Friends</Text>
+        <List containerStyle={{ marginBottom: 20, alignSelf: 'stretch' }}>
+          {
+            this.state.data.map((l, i) => (
+              <ListItem
+                roundAvatar
+                avatar={require('../assets/user2.jpg')}
+                key={i}
+                title={l.name}
+                subtitle={l.email}
+              />
+            ))
+          }
+        </List>
+        <Button
+          onPress={() => this.GetFriendList()}  
+          title="Get Friendlist"/>
       </View>
     )
   }
